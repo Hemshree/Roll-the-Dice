2fdc5b4fac23a5fedade37c64b8505208b808823
Hemshree <31439735+Hemshree100@users.noreply.github.com>
2020-03-08 22:10:28 +0530

    Added files

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.purpleAccent,
+        appBar: AppBar(
+          title: Text('Roll the Dice'),
+          backgroundColor: Colors.purpleAccent,
+        
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber= 1;
+  int rightDiceNumber= 1;
+
+  void changeDiceFace()
+  {
+    setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                  rightDiceNumber = Random().nextInt(6) + 1; 
+                });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+          
+            child: FlatButton(
+              onPressed: (){
+               changeDiceFace();
+              },
+                            child: Image(image: AssetImage('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+            ),
+        
+         Expanded(
+           flex: 1,
+           child: FlatButton(
+             onPressed: (){
+              changeDiceFace();
+             },
+                          child: Image(image: AssetImage('images/dice$rightDiceNumber.png'),
+             ),
+           ),
+           ),
+        
+        ],
+      ),
+
+
+    );
+  }
+}
+
